77bf1f0535f4a8a0a57e07a0f9b49f5f2b86b4df
Thomas Guth <thomas.guth@hotmail.fr>
2021-03-24 10:16:40 +0100

    Update test4.js

--- test4.js
+++ test4.js
@@ -2,7 +2,9 @@ var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
     if (this.readyState==XMLHttpRequest.DONE && this.status==200){
         var response=JSON.parse(this.responseText);
-        console.log(response);
+        let carte=document.getElementById("main");
+        let carte2=document.getElementById("spani");
+        carte2.innerText="response.name";
     }
 };
 request.open("GET","localhost/3000/api/teddies");
