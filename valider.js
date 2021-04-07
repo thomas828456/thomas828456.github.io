d89805953cd89a26d20cae798ebc59313d9d68fe
Thomas Guth <thomas.guth@hotmail.fr>
2021-04-07 16:18:09 +0200

    p1

--- valider.js
+++ valider.js
@@ -2,3 +2,4 @@ let text=document.getElementById("text");
 let a=localStorage.getItem("valider").split("#")[0];
 let b=localStorage.getItem("valider").split("#")[1];
 text.innerText="votre commande d'un montant de : "+a+", porte le numéro : "+b;
+localStorage.clear();
